29a12e7929d3fc459cab907af55e47ab35256b40
Siva Prasanth <144767115+sivaprasanth2221@users.noreply.github.com>
2024-11-08 11:43:35 +0530

    Update stream-csv-to-kafka.sh

--- ex10/stream-csv-to-kafka.sh
+++ ex10/stream-csv-to-kafka.sh
@@ -1,5 +1,5 @@
 # stream-csv-to-kafka.sh
 # Adjust the file path to your CSV file
 while IFS= read -r line; do
-    echo "$line" | kafka-console-producer --topic test-stream --bootstrap-server localhost:9092
-done < /Users/sivaprasanth/Documents/Big\ Data/ex10/streaming_data.csv
+    echo "$line" | bin/kafka-console-producer.sh --topic test-stream --bootstrap-server localhost:9092
+done < streaming_data.csv
